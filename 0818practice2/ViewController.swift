ca0105842102ae1453aa055d53974889e4dc5547
LinJY0424 <f0983344166@gmail.com>
2020-08-18 20:45:35 +0800

    Initial Commit

---
+++ 0818practice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0818practice2
+//
+//  Created by Duc-Thang Vo on 2020/8/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
